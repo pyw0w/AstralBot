fada0b8471d70bc3d871fcaed0b2d04921a71482
PyWhy <myxi2002@gmail.com>
2024-11-10 15:04:08 +0500

    Удаление устаревшего логгирования и добавление функции получения случайного названия из Anilibria, обновление команды теста для обработки аргументов

--- internal/commands/steaminfo.go
+++ internal/commands/steaminfo.go
@@ -3,59 +3,8 @@ package commands
 import (
 	"AstralBot/internal/steam"
 	"fmt"
-	"io"
-	"log"
-	"os"
 	"strings"
 )
-
-type Logger struct {
-	debug *log.Logger
-	info  *log.Logger
-	error *log.Logger
-}
-
-type LoggerAdapter struct {
-	logger *Logger
-	source string
-}
-
-func (l *LoggerAdapter) Write(p []byte) (n int, err error) {
-	// Удаляем лишние символы новой строки
-	message := string(p)
-	message = strings.TrimSuffix(message, "\n")
-	l.logger.Debug(l.source, message)
-	return len(p), nil
-}
-
-func (l *Logger) Debug(source string, v ...interface{}) {
-	message := fmt.Sprint(v...)
-	l.debug.Printf("[%s] %s", source, message)
-}
-
-func (l *Logger) NewAdapter(source string) io.Writer {
-	return &LoggerAdapter{
-		logger: l,
-		source: source,
-	}
-}
-
-func NewLogger(debugMode bool) *Logger {
-	var debugOutput io.Writer
-	if debugMode {
-		debugOutput = os.Stdout
-	} else {
-		debugOutput = io.Discard
-	}
-
-	logger := &Logger{
-		debug: log.New(debugOutput, "DEBUG:", log.Ldate|log.Ltime),
-		info:  log.New(os.Stdout, "INFO:", log.Ldate|log.Ltime),
-		error: log.New(os.Stderr, "ERROR:", log.Ldate|log.Ltime),
-	}
-
-	return logger
-}
 
 func RegisterSteamInfoCommand(cmdHandler *CommandHandler) {
 	cmd := Command{
--- internal/commands/test.go
+++ internal/commands/test.go
@@ -1,11 +1,21 @@
 package commands
 
+import (
+	"AstralBot/internal/anilibria"
+	"fmt"
+)
+
 func RegisterTestCommand(cmdHandler *CommandHandler) {
 	cmd := Command{
 		Name:        "test",
 		Description: "",
 		Execute: func(args []string) (string, error) {
-			return "Test!", nil
+			if len(args) < 1 {
+				return "❌", nil
+			}
+
+			fmt.Print(anilibria.GetTitle())
+			return "Test!" + args[0], nil
 		},
 	}
 
