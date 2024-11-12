1df5ec0e7d64bef85baa75d81607829b64d0495b
PyWhy <myxi2002@gmail.com>
2024-11-12 15:52:38 +0500

    Добавление поддержки сжатия Gzip в сервере: интеграция gin-contrib/gzip и настройка роутера для использования Gzip.

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.6 // indirect
+	github.com/gin-contrib/gzip v1.0.1
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
--- web/server.go
+++ web/server.go
@@ -6,6 +6,7 @@ import (
 	"AstralBot/internal/logger"
 	"fmt"
 
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,8 +31,13 @@ func NewServer(cfg *config.Config, logger *logger.Logger) *Server {
 var router *gin.Engine
 
 func (s *Server) Start() {
+	// Set the Gin mode to release
+	if !s.config.DebugMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	// Создаем новый роутер
-	router = gin.Default()
+	router = gin.New()
+	router.Use(gzip.Gzip(gzip.BestSpeed))
 
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
