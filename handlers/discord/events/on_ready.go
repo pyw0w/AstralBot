45e0a67114c12622eccf6d99f36ba057a91aa56e
PyWhy <myxi2002@gmail.com>
2024-11-14 18:29:13 +0500

    Обновление статуса бота: добавлено отображение количества уникальных пользователей и времени работы в статусе.

--- handlers/discord/events/on_ready.go
+++ handlers/discord/events/on_ready.go
@@ -1,9 +1,28 @@
 package events
 
 import (
+	"fmt"
+
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 )
 
+var (
+	startTime    = time.Now()
+	commandCount int
+)
+
 func OnReady(s *discordgo.Session, r *discordgo.Ready) {
-	s.UpdateCustomStatus("🤖")
+	// Calculate the number of unique users
+	uniqueUsers := make(map[string]bool)
+	for _, guild := range s.State.Guilds {
+		for _, member := range guild.Members {
+			uniqueUsers[member.User.ID] = true
+		}
+	}
+
+	// Update the status with the number of unique users
+	status := fmt.Sprintf("🤖 Commands: %d | Uptime: %s", commandCount, time.Since(startTime).String())
+	s.UpdateCustomStatus(status)
 }
