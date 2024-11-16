56de218d2bd9535db2190ceeab2a3cfce9061255
PyWhy <myxi2002@gmail.com>
2024-11-16 16:20:58 +0500

    Упрощение структуры конфигурации: удаление комментариев и приведение к единому стилю форматирования полей в структуре Config.

--- utils/config/config.go
+++ utils/config/config.go
@@ -8,17 +8,15 @@ import (
 )
 
 type Config struct {
-	TelegramToken      string
-	DiscordToken       string
-	DebugMode          bool
-	CommandPrefix      string
-	DetailedAPILogs    bool
-	SteamAPIKey        string
-	WebPort            int
-	DBType             string // Тип базы данных (например, "mongodb", "sqlite", "mysql")
-	DBConnectionString string // Строка подключения к базе данных
-	DiscordChannelID   string
-	AccessToken        string
+	TelegramToken    string
+	DiscordToken     string
+	DebugMode        bool
+	CommandPrefix    string
+	DetailedAPILogs  bool
+	SteamAPIKey      string
+	WebPort          int
+	DiscordChannelID string
+	AccessToken      string
 }
 
 func LoadConfig() *Config {
@@ -28,16 +26,14 @@ func LoadConfig() *Config {
 	}
 
 	return &Config{
-		TelegramToken:      os.Getenv("TELEGRAM_TOKEN"),
-		DiscordToken:       os.Getenv("DISCORD_TOKEN"),
-		DebugMode:          os.Getenv("DEBUG_MODE") == "true",
-		CommandPrefix:      os.Getenv("COMMAND_PREFIX"),
-		DetailedAPILogs:    os.Getenv("DETAILED_API_LOGS") == "true",
-		SteamAPIKey:        os.Getenv("STEAM_API_KEY"),
-		WebPort:            8080,
-		DBType:             os.Getenv("DB_TYPE"),
-		DBConnectionString: os.Getenv("DB_CONNECTION_STRING"),
-		DiscordChannelID:   os.Getenv("DISCORD_CHANNEL_ID"),
-		AccessToken:        os.Getenv("ACCESS_TOKEN"),
+		TelegramToken:    os.Getenv("TELEGRAM_TOKEN"),
+		DiscordToken:     os.Getenv("DISCORD_TOKEN"),
+		DebugMode:        os.Getenv("DEBUG_MODE") == "true",
+		CommandPrefix:    os.Getenv("COMMAND_PREFIX"),
+		DetailedAPILogs:  os.Getenv("DETAILED_API_LOGS") == "true",
+		SteamAPIKey:      os.Getenv("STEAM_API_KEY"),
+		WebPort:          8080,
+		DiscordChannelID: os.Getenv("DISCORD_CHANNEL_ID"),
+		AccessToken:      os.Getenv("ACCESS_TOKEN"),
 	}
 }
